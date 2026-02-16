8d84314b06284a47060966e41f3e0bcd8b865225
DisisuPeft <defp240899@gmail.com>
2026-02-16 08:35:36 -0600

    save changes

--- build.sh
+++ build.sh
@@ -2,8 +2,8 @@
 # set -o errexit  # si un comando falla, detener todo el script
 
 # 1. Instalar dependencias
-# pip install --upgrade pip
-# pip install -r requeriments.txt
+ pip install --upgrade pip
+ pip install -r requeriments.txt
 
 # 2. Ejecutar migraciones
 # python manage.py makemigrations --noinput
